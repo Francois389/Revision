1bf6a257c609d5564bf9834162b18cc195c5c42d
François SP <francois-sp@gmx.fr>
2024-02-24 18:47:27 +0100

    AJout de commentaire

--- src/other/js/gestionEtoile.js
+++ src/other/js/gestionEtoile.js
@@ -1,19 +1,27 @@
+/* Script permettant de gérer la selection d'importance. */
+
+/* 'radio-group' est l'élément qui regroupe toutes les radios */
 const radioGroup = document.getElementById("radio-group");
 const tabRadio = radioGroup.children;
+/* 'valeur-importance' est l'élément qui contient la valeur de l'importance pour la faire passer dans un formulaire */
 const valeurImportance = document.getElementById("valeur-importance");
 
-let cpt = 0;
 for (const radio of tabRadio) {
     radio.addEventListener("click", (radio)=>{handleClick(radio);});
-    cpt ++;
 }
 
-console.log(valeurImportance.value)
+// Si une valeur d'importance est déjà présente, on déclenche le click sur la radio correspondante
+// pour que le formulaire soit cohérent avec la valeur déjà présente
 if (valeurImportance.value !== "") {
     tabRadio[valeurImportance.value - 1].click();
 }
 
-function handleClick(elementClick, indiceDansListe) {
+/**
+ * Fonction permettant de gérer le click sur un élément radio.
+ * Quand on clique sur un élément radio, on met à jour la valeur d'importance et on coche les éléments radios inférieurs
+ * @param elementClick élément sur lequel on a cliqué
+ */
+function handleClick(elementClick) {
     elementClick = elementClick.target;
     valeurImportance.value = elementClick.value;
     const indice = getIndiceFromList(tabRadio, elementClick);
@@ -25,6 +33,12 @@ function handleClick(elementClick, indiceDansListe) {
     }
 }
 
+/**
+ * Récupérer l'indice d'un élément dans une liste
+ * @param liste liste dans laquelle on recherche
+ * @param elementRecherche élément que l'on recherche
+ * @returns {number} l'indice de l'élément dans la liste, -1 si l'élément n'est pas dans la liste
+ */
 function getIndiceFromList(liste, elementRecherche) {
     let resultat = -1;
     for (let i = 0; i < liste.length && resultat === -1; i++) {
